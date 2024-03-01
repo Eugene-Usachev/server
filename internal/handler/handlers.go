e28755346ccbbd1f176a485d613b264adf631012
Eugene-Usachev <87601294+Eugene-Usachev@users.noreply.github.com>
2024-03-01 16:22:48 +0300

    Update handlers.go

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -46,11 +46,7 @@ func NewHandler(cfg *HandlerConfig) *Handler {
 func (handler *Handler) InitMiddlewares(app *fiber.App) {
 
 	app.Use(func(c *fiber.Ctx) error {
-		if fastbytes.Equal(c.Request().Header.Referer(), []byte("http://localhost:3000/")) {
-			c.Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		} else {
-			c.Set("Access-Control-Allow-Origin", "https://client-olaz.onrender.com")
-		}
+		c.Set("Access-Control-Allow-Origin", "https://client-olaz.onrender.com")
 		c.Set("Access-Control-Allow-Credentials", "true")
 		c.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
